30a326a4edf156f4fb7850ed55f608bec1b7a7f7
306327680 <306327680@qq.com>
2018-08-16 11:08:13 +0800

    ..

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 It used to detect whether a driver is tierd
 It use haar detector to detect the face first and to set a ROI, in ROI it detect the right eye and closed eye. By counting 
 the percent of closed eyes to detect whether the driver is tired.
+
+![pic](pic/1909704145.jpg)
---
+++ build/CMakeFiles/tired_detction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tired_detction.dir/main.cpp.o"
+  "tired_detction.pdb"
+  "tired_detction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tired_detction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
